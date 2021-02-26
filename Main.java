4b6e427b8b750a605adbe8b5e901038affa0ba79
Treyway1 <treyvor.romero-jones@patelhs.org>
2021-02-26 13:11:39 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    
+    System.out.println("Welcome to 4kTrey's Sandal Store!! We have shoes from every company and brand for a reasonable price. Also we have a full variety of color ways and slide colors to choose from.");
+    System.out.println("what is the cost of the sandal you're buying?");
+    int sandalCost = scan.nextInt();
+    System.out.println("How many sandals are you buying");
+    int sandalAmount = scan.nextInt();
+
+        System.out.println("Your change is " +affordable(sandalAmount, sandalCost));
+  }
+  public static int sandalCost(int sandalAmount){
+    //Take sandalAmount and amountOfShoes and write a method that calculates cost.
+
+    int sandalCost = int sandalAmount;
+
+    if(totalCost > 200);
+
+    return int sandalAmount + sandalCost(totalCost - 50);
+
+    }else{
+    return sandalCost;
+  }
+}
